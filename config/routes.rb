5f67b0b25631cfb82513416ced49bd2defcd7073
Slade Watkins <srw@sladewatkins.net>
2023-07-07 09:13:20 -0400

    routes: remove unnecessary stuff from base merge
    Signed-off-by: Slade Watkins <srw@sladewatkins.net>

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
 Rails.application.routes.draw do
   resources :articles
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  resources :article, except: [:edit, :update]
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 end
